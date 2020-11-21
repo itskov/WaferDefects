310bb3a1e57485810c768017ce7878a275108d6c
itskov <itskov@gmail.com>
2020-11-21 21:13:17 +0200

    GMM

--- WDGmm.py
+++ WDGmm.py
@@ -1,28 +1,38 @@
-def train_gmm(case, prp):
-  ref_img = plt.imread(prp.samples_dict[case].reference, 0)
-  orig_shape = ref_img.shape
+import matplotlib.pyplot as plt
 
-  ref_image_flat = np.reshape(ref_img, (-1, 1))
-  gmm_model = GMM(n_components=3, covariance_type='diag').fit(np.reshape(ref_image_flat, (-1, 1)))
-  return gmm_model
 
-def predict_gmm(case, gmm_mode, prp, is_ref=True):
-  img = plt.imread(prp.samples_dict[case].reference, 0) if is_ref \
-        else plt.imread(prp.samples_dict[case].inspected, 0)
+class WDGmm:
+	def __init__(self):
+		self.gmm_model = None
+	
 
-  orig_shape = img.shape
-  img_flat = np.reshape(img, (-1, 1))
+	def train_gmm(case, prp):
+	  ref_img = plt.imread(prp.samples_dict[case].reference, 0)
+	  orig_shape = ref_img.shape
 
-  gmm_predict_labels = gmm_model.predict(img_flat)
-  gmm_predict_probs = gmm_model.predict_proba(img_flat)
+	  ref_image_flat = np.reshape(ref_img, (-1, 1))
+	  self.gmm_model = GMM(n_components=3, covariance_type='diag').fit(np.reshape(ref_image_flat, (-1, 1)))
+	 
 
-  probs_image = np.array([gmm_predict_probs[i,gmm_predict_labels[i]] for i in range(img_flat.size)])
+	def predict_gmm(case, gmm_mode, prp, is_ref=True):
+	  asser(self.gmm_model is not None)
 
-  print(gmm_predict_labels.shape)
-  print(probs_image.shape)
+	  img = plt.imread(prp.samples_dict[case].reference, 0) if is_ref \
+		else plt.imread(prp.samples_dict[case].inspected, 0)
 
-  gmm_predict_labels = np.reshape(gmm_predict_labels, orig_shape)
-  probs_image = np.reshape(probs_image, orig_shape)
+	  orig_shape = img.shape
+	  img_flat = np.reshape(img, (-1, 1))
 
-  return(gmm_predict_labels, probs_image)
+	  gmm_predict_labels = self.gmm_model.predict(img_flat)
+	  gmm_predict_probs = self.gmm_model.predict_proba(img_flat)
+
+	  probs_image = np.array([gmm_predict_probs[i,gmm_predict_labels[i]] for i in range(img_flat.size)])
+
+	  print(gmm_predict_labels.shape)
+	  print(probs_image.shape)
+
+	  gmm_predict_labels = np.reshape(gmm_predict_labels, orig_shape)
+	  probs_image = np.reshape(probs_image, orig_shape)
+
+	  return(gmm_predict_labels, probs_image)
 
